d427af00e6db78a68b78233ceaa54d1c17b10c23
danny <dannymamusha@gmail.com>
2021-08-18 01:30:18 -0400

    deployment

--- server.js
+++ server.js
@@ -16,7 +16,15 @@ app.use(cors());
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname,"client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+} else{
+  app.get("/", (req, res) =>{
+    res.send("api running");
+  })
 }
 
 
